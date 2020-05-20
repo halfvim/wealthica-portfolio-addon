ad89c5269260e52a59bd1428664ac91d1bbd6480
Yan Chen <yan.chen.im@gmail.com>
2020-05-20 18:42:14 -0400

    init

---
+++ precache-manifest.7fe32b23a8b128bc658d5d4b5baba17d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e93b74dc48887aa7b63",
+    "url": "/wealthica-portfolio-addon/static/css/main.eb45ee7f.chunk.css"
+  },
+  {
+    "revision": "4e93b74dc48887aa7b63",
+    "url": "/wealthica-portfolio-addon/static/js/main.4e93b74d.chunk.js"
+  },
+  {
+    "revision": "bae5cf18035738988b3a",
+    "url": "/wealthica-portfolio-addon/static/js/runtime~main.bae5cf18.js"
+  },
+  {
+    "revision": "c8df27e20ca0c9b433be",
+    "url": "/wealthica-portfolio-addon/static/css/2.a7f7df27.chunk.css"
+  },
+  {
+    "revision": "c8df27e20ca0c9b433be",
+    "url": "/wealthica-portfolio-addon/static/js/2.c8df27e2.chunk.js"
+  },
+  {
+    "revision": "910b5290ea875ba878a834eb29dfc89a",
+    "url": "/wealthica-portfolio-addon/index.html"
+  }
+];
